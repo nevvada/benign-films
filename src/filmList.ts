19179d619839ef9e1860717707ef80ebd2cf26ce
nlemast <nlemaster47@gmail.com>
2021-03-04 12:14:59 -0500

    Create film list with DomiNYC film date, info, image, and title; create renderFilmList function to iterate over films and create jsx

---
+++ src/filmList.ts
@@ -0,0 +1,17 @@
+interface Film {
+  date: string;
+  info: string;
+  imageSrc: string;
+  title: string;
+}
+
+const filmList: Film[] = [
+  {
+    date: 'TBD',
+    info: 'a spooky scare',
+    imageSrc: 'https://res.cloudinary.com/dwcrpofnt/image/upload/v1614877506/benign-films/dominyc-poster_km33at.png',
+    title: 'DomiNYC',
+  }
+];
+
+export default filmList;
--- src/pages/films.tsx
+++ src/pages/films.tsx
@@ -1,10 +1,34 @@
+import Image from 'next/image';
 import React from 'react';
 
 import Layout from '../components/Layout/Layout';
 
+import filmList from '../filmList';
+
+const renderFilmList = (films) => (
+  films.map(film => {
+    const { date, imageSrc, info, title} = film;
+
+    return (
+      <>
+        <h1>{title}</h1>
+        <h4>{date}</h4>
+        <p>{info}</p>
+        <Image
+          height={600}
+          src={imageSrc}
+          width={600}
+        />
+      </>
+    )
+  })
+);
+
 const Films: React.FC = () => (
   <Layout>
-    filmsss
+    <section>
+      {renderFilmList(filmList)}
+    </section>
   </Layout>
 );
 
