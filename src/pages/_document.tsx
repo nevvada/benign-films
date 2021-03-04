9663d07d3cbbac4cba901a6aee82fb2681f19b69
nlemast <nlemaster47@gmail.com>
2021-03-04 17:36:24 -0500

    Change global font; play around with background colors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
