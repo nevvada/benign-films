6eef8318a1ba35ccad00bb5afee02ef3930a87d2
nlemast <nlemaster47@gmail.com>
2021-03-04 13:08:23 -0500

    Add support for active class being conditionally added to nav list items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,35 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import styles from './Header.module.scss';
 
-const Header: React.FC = () => (
-  <nav className={styles.nav}>
-    <Link href="/films">
-      films
-    </Link>
-    <Link href="/about">
-      about
-    </Link>
-    <Link href="/contact">
-      contact
-    </Link>
-  </nav> 
-);
+const FILMS_PATHNAME = '/films';
+const ABOUT_PATHNAME = '/about';
+const CONTACT_PATHNAME = '/contact';
+
+const Header: React.FC = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <ul className={styles.navBar}>
+      <li {...(pathname === FILMS_PATHNAME && { className: styles.activeRoute })}>
+        <Link href={FILMS_PATHNAME}>
+          films
+        </Link>
+      </li>
+      <li {...(pathname === ABOUT_PATHNAME && { className: styles.activeRoute })}>
+        <Link href={ABOUT_PATHNAME}>
+          about
+        </Link>
+      </li>
+      <li {...(pathname === CONTACT_PATHNAME && { className: styles.activeRoute })}>
+        <Link href={CONTACT_PATHNAME}>
+          contact
+        </Link>
+      </li>
+    </ul>
+  );
+};
 
 export default Header;
