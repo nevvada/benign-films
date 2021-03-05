9c4bdc1ccb4ce3b69ed829fa37cda3558334b9d3
nlemast <nlemaster47@gmail.com>
2021-03-05 11:04:44 -0500

    Add responsiveness to home page text and films

--- src/filmList.ts
+++ src/filmList.ts
@@ -11,7 +11,7 @@ const filmList: Film[] = [
     description: 'a spooky scare, the scariest of the spooks, the spookiest of the scares',
     imageSrc: 'https://res.cloudinary.com/dwcrpofnt/image/upload/v1614877506/benign-films/dominyc-poster_km33at.png',
     title: 'DomiNYC',
-  }
+  },
 ];
 
 export default filmList;
